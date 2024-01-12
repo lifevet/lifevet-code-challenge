269ea2e73d5a8cb3393ae3d7301143d457bf68f9
Kayo Oliveira <g.kayooliveira123@gmail.com>
2024-01-12 19:08:05 -0300

    Add exams.json and exams.sql files

---
+++ exams.sql
@@ -0,0 +1,23 @@
+INSERT INTO exam_types (id, name) VALUES
+(1, 'Biologia Molecular(PCR)'),
+(2, 'Bioquímica'),
+(3, 'Hematologia'),
+(4, 'Imunologia');
+
+INSERT INTO exam (id, NAME, exam_type_id) VALUES
+(1, 'Babesia SPP(PCR Real Time)', 1),
+(2, 'Cinomose(PCR Real Time)', 1),
+(3, 'Ehrlichia SPP(PCR Real Time)', 1),
+(4, 'Leishmania spp. - PCR Real Time', 1),
+(5, 'Ácido Úrico', 2),
+(6, 'Albumina', 2),
+(7, 'ALT (TGP)', 2),
+(8, 'Amilase', 2),
+(9, 'Pesquisa Seriada de Hemocitozoários (PSH)', 3),
+(10, 'Teste de Coagulação (PT e APTT)', 3),
+(11, 'Teste de Compatibilidade Sanguínea', 3),
+(12, 'Teste para Anemia Hemolítica Imunomediada', 3),
+(13, '4DX - IgG elisa (Ehrlichia+Borrelia+Dirofilária)', 4),
+(14, 'Cinomose + Parvovirose - Elisa IgG Dot Blot', 4),
+(15, 'Cinomose + Parvovirose - Elisa IgM Dot Blot', 4),
+(16, 'Dirofilariose', 4);
